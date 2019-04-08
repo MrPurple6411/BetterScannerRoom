ce7548633c27b5ca89f56f2ed1c99599b7a46f35
Dolphin <Darth-Cthoras@users.noreply.github.com>
2019-04-07 21:23:32 -0400

    Update QMultiModSettings.cs

--- QMultiMod/QMultiModSettings.cs
+++ QMultiMod/QMultiModSettings.cs
@@ -6,45 +6,6 @@ namespace QMultiMod
 {
     class QMultiModSettings
     {
-        public float DayNightSpeed = 1f;
-        public float AttackLastTargetSwimVelocity = 2f;
-        public float AttackLastTargetCrawlVelocity = 2f;
-        public float SpotlightPowerPerSecond = 0.05f;
-        public float SpotlightUpdateInterval = 20f;
-        public float TechlightPowerPerSecond = 0.05f;
-        public float TechlightUpdateInterval = 20f;
-        public int MinDrillableAmount = 2;
-        public int MaxDrillableAmount = 5;
-        public float DrillableSpawnChanceMultiplier = 2f;
-        public int ExosuitStorageWidth = 6;
-        public int ExosuitStorageHeight = 4;
-        public int ExosuitWidthPerStorageModule = 0;
-        public int ExosuitHeightPerStorageModule = 1;
-        public float ExosuitThrustConsumption = 0.04f;
-        public float ExosuitClawDamageToLiving = 100f;
-        public float ExosuitDrillDamageToLiving = 16f;
-        public float ExosuitGrapplingArmRange = 70f;
-        public float ExosuitGrapplingArmDamage = 0f;
-        public int GravsphereMaxTrapped = 24;
-        public float GravsphereMaxForce = 30f;
-        public int PlayerInventoryWidth = 8;
-        public int PlayerInventoryHeight = 8;
-        public float PlayerDamageTakenMultiplier = 1f;
-        public float VehicleDamageTakenMultiplier = 2f;
-        public float SubDamageTakenMultiplier = 2f;
-        public bool StorageContainersStack = true;
-        public float SeamothReinforcementModuleDepth = 1600f;
-        public float SeamothHullMarkOneDepth = 100f;
-        public float SeamothHullMarkTwoDepth = 300f;
-        public float SeamothHullMarkThreeDepth = 1500f;
-        public int SeamothStorageContainerWidth = 6;
-        public int SeamothStorageContainerHeight = 6;
-        public float CyclopsThermalReactorEfficiency = 2.0f;
-        public float CyclopsTurningTorqueMultiplier = 1.5f;
-        public float CyclopsForwardAccelMultiplier = 1.5f;
-        public float CyclopsVerticalAccelMultiplier = 1.0f;
-        public float SwimToMeatVelocity = 8f;
-        public float VehicleForwardForceMultiplier = 1.5f;
         public float ScannerSpeedNormalInterval = 14f;
         public float ScannerSpeedMinimumInterval = 1f;
         public float ScannerSpeedIntervalPerModule = 3f;
@@ -52,10 +13,6 @@ namespace QMultiMod
         public float ScannerMinRange = 600f;
         public float ScannerUpgradeAddedRange = 100f;
         public float ScannerCameraRange = 1000f;
-        public bool NitrogenEnabled = false;
-        public bool UnlockHullPlates = false;
-        public bool AllowAchievementsWithConsole = false;
-        public float SwimChargeFinsModifier = 0.005f;
         public float FireExtinguisherHolderRechargeValue = 0.005f;
 
 
