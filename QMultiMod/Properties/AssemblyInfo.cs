00d58fc48c67dc5cd37db904227067acdfa107d0
Dolphin <Darth-Cthoras@users.noreply.github.com>
2019-04-07 22:00:08 -0400

    Update AssemblyInfo.cs

--- QMultiMod/Properties/AssemblyInfo.cs
+++ QMultiMod/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("QMultiMod")]
+[assembly: AssemblyTitle("BetterScannerRoom")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("QMultiMod")]
+[assembly: AssemblyProduct("BetterScannerRoom")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
