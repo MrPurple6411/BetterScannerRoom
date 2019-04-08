e3bd55cf573034a172eda5c27d365a2c878063dc
Dolphin <Darth-Cthoras@users.noreply.github.com>
2019-04-07 22:28:58 -0400

    Update AssemblyInfo.cs

--- BetterScannerRoom/Properties/AssemblyInfo.cs
+++ BetterScannerRoom/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BetterScannerRoom")]
-[assembly: AssemblyDescription("BetterScannerRoom by Qwiso (formerly QMultiMod")]
+[assembly: AssemblyDescription("BetterScannerRoom by Qwiso - formerly QMultiMod")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BetterScannerRoom")]
