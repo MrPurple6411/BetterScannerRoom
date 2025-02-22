a2f66cb6e2ed6b2a91a2e3a16c7f2d01a5a3d94c
Joshua Gibbs <mrpurple6411@gmail.com>
2025-02-22 13:00:43 -0700

    process checks

--- BetterScannerRoom/Plugin.cs
+++ BetterScannerRoom/Plugin.cs
@@ -15,6 +15,11 @@ using UnityEngine;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 [BepInDependency(Nautilus.PluginInfo.PLUGIN_GUID, Nautilus.PluginInfo.PLUGIN_VERSION)]
 [BepInIncompatibility("com.ahk1221.smlhelper")]
+#if SUBNAUTICA
+[BepInProcess("Subnautica.exe")]
+#elif BELOWZERO
+[BepInProcess("SubnauticaZero.exe")]
+#endif
 [HarmonyPatch]
 public class Plugin : BaseUnityPlugin
 {
